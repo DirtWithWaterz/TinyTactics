f47b6c5bf6fed6634a8b29dc930b909b1c967fa6
Barbra <joshturbessi@gmail.com>
2023-05-02 19:56:18 -0500

    Made Tile Player Function

--- TinyTactics/Assets/Game/Scripts/Game/TilePlacer.cs
+++ TinyTactics/Assets/Game/Scripts/Game/TilePlacer.cs
@@ -7,31 +7,65 @@ public class TilePlacer : MonoBehaviourPun
     public TileBase[] TileBases;
     public Transform[] TileDataObjects;
     [SerializeField] private string selectedTile = null;
-    [SerializeField] private Tilemap tilemap = null;
+    [SerializeField] private byte width;
+    [SerializeField] private Tilemap buildingsMap = null;
+    [SerializeField] private Tilemap groundMap = null;
     [SerializeField] private GameObject previewTile = null;
 
     public void Start()
     {
-        tilemap = GameObject.Find("Buildings").GetComponent<Tilemap>();
+        buildingsMap = GameObject.Find("Buildings").GetComponent<Tilemap>();
+        groundMap = GameObject.Find("Ground").GetComponent<Tilemap>();
     }
     private void Update()
     {
         if (selectedTile != null && UserInput.instance.Interact)
         {
-            photonView.RPC(nameof(SetTileRPC), RpcTarget.AllBufferedViaServer, UserInput.instance.MousePosition, selectedTile);
+            photonView.RPC(nameof(SetTileRPC), RpcTarget.AllBufferedViaServer, UserInput.instance.MousePosition, selectedTile, width);
+            Debug.Log(selectedTile);
+            Debug.Log(width);
         }
     }
     public void SelectTile(string tileName)
     {
-        selectedTile = tileName;
+        char lastChar = tileName[tileName.Length - 1];
+        if (char.IsDigit(lastChar))
+        {
+            string str = lastChar.ToString();
+            width = byte.Parse(str);
+            selectedTile = tileName.Substring(0, tileName.Length - 1);
+        }
+
     }
     [PunRPC]
-    public void SetTileRPC(Vector3Int pos, string _string)
+    public void SetTileRPC(Vector3 pos, string _string, byte width)
     {
         foreach (TileBase tile in TileBases)
         {
             if (tile.name != _string) { continue; }
-            tilemap.SetTile(pos, tile);
+            Vector3Int position = new Vector3Int(Mathf.FloorToInt(pos.x * 2), Mathf.FloorToInt(pos.y * 2), 0);
+            if (groundMap.HasTile(position)) { return; }
+            if (!groundMap.HasTile(new Vector3Int(position.x, position.y - 1, 0))) { return; }
+            bool colliderFound = false;
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = -1; j <= 1; j += 2)
+                {
+                    Vector3 worldPosition = buildingsMap.CellToWorld(new Vector3Int(position.x + (i * j), position.y, 0));
+                    Collider2D collider = Physics2D.OverlapPoint(worldPosition);
+                    if (collider != null)
+                    {
+                        colliderFound = true;
+                        break;
+                    }
+                }
+                if (colliderFound) { break; }
+            }
+
+            if (!colliderFound)
+            {
+                buildingsMap.SetTile(position, tile);
+            }
         }
     }
 }
--- TinyTactics/Assets/Game/Scripts/UserInput.cs
+++ TinyTactics/Assets/Game/Scripts/UserInput.cs
@@ -14,6 +14,7 @@ public class UserInput : MonoBehaviourPun
     [SerializeField] private Camera cam;
     private PlayerInput _playerInput;
     private InputAction _panbutton;
+    private InputAction _mouseInput;
     private InputAction _mousepos;
     private InputAction _interactbutton;
 
@@ -28,12 +29,14 @@ public class UserInput : MonoBehaviourPun
     {
         _panbutton = _playerInput.actions["PanButton"];
         _interactbutton = _playerInput.actions["Interact"];
+        _mouseInput = _playerInput.actions["MouseInput"];
         _mousepos = _playerInput.actions["MousePos"];
     }
     private void Update()
     {  
         Panning = _panbutton.IsPressed();
         Interact = _interactbutton.WasPressedThisFrame();
+        MouseInput = _mouseInput.ReadValue<Vector2>();
         Vector2 rawMousePos = _mousepos.ReadValue<Vector2>();
         MousePosition = cam.ScreenToWorldPoint(new Vector3(rawMousePos.x, rawMousePos.y, 0));
     }
